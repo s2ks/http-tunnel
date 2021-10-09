54e20c342af212a1f2c63dc384052298aa06dbe2
Jilles Bakker <bakkerjilles@gmail.com>
2021-10-09 23:49:48 +0200

    syntax errors

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,8 +8,8 @@ import (
 	"encoding/base64"
 	"strings"
 
-	"github.com/s2ks/http-tunnel/internal/config"
-	"github.com/s2ks/http-tunnel/internal/encoding"
+	//tunnel_config "github.com/s2ks/http-tunnel/internal/config"
+	tunnel_encoding "github.com/s2ks/http-tunnel/internal/encoding"
 )
 
 
@@ -18,11 +18,16 @@ client and the server having accept/connect options
 to specify on what address to accept the connection and
 to what address to send the encoded/decoded packet to. */
 
-connect_option := flag.String("connect", "", "Address to connect to")
-protocol_option := flag.String("protocol", "tcp",
-	"Network protocol to use: tcp/tcp4/tcp6/unix/unixpacket")
-accept_option := flag.String("accept", "localhost:12345",
-	"Address to listen on: [host]:port")
+var (
+	connect_option 	= flag.String("connect", "", "Address to connect to")
+	protocol_option = flag.String("protocol", "tcp",
+		"Network protocol to use: tcp/tcp4/tcp6/unix/unixpacket")
+	accept_option = flag.String("accept", "localhost:12345",
+		"Address to listen on: [host]:port")
+
+	/* TODO default search path for config file */
+	config_file_option = flag.String("config", "", "Path to a configuration file")
+)
 
 func Handle(conn net.Conn) {
 	buf := make([]byte, 512)
@@ -30,9 +35,9 @@ func Handle(conn net.Conn) {
 	log.Print(conn.RemoteAddr().String())
 	log.Print(conn.LocalAddr().String())
 
-	for n, err := conn.Read(buf); n > 0 {
-		if err {
-			log.Error(err)
+	for n, err := conn.Read(buf); n > 0; {
+		if err != nil {
+			log.Print(err)
 			return
 		}
 
@@ -40,16 +45,16 @@ func Handle(conn net.Conn) {
 
 		/* Send a POST with the base64 encoded original request
 		to the host specified by 'connect_option' */
-		resp, err := http.Post(connect_option, "text/plain",
+		resp, err := http.Post(*connect_option, "text/plain",
 			strings.NewReader(b64data))
 
 		if err != nil {
-			log.Error(err)
+			log.Print(err)
 			return
 		}
 
 		if resp.ContentLength == 0 {
-			log.Error("ContentLength = 0; no data")
+			log.Print("ContentLength = 0; no data")
 			return
 		}
 
@@ -57,16 +62,18 @@ func Handle(conn net.Conn) {
 		rn, err := resp.Body.Read(respbuf)
 
 		if err != nil {
-			log.Error(err)
+			log.Print(err)
 			return
 		}
 
 		if rn == 0 {
-			log.Error("No data")
+			log.Print("No data")
 			return
 		}
 
-		decoded, err := decoder.Decode(respbuf)
+		decoded, err := tunnel_encoding.Decode(respbuf)
+
+		log.Print(decoded)
 
 		/* TODO send the decoded packet back to the remote */
 		//conn.RemoteAddr().String()
@@ -76,7 +83,7 @@ func Handle(conn net.Conn) {
 func main() {
 	flag.Parse()
 
-	listener, err := net.Listen(proto, listenaddr)
+	listener, err := net.Listen(*protocol_option, *accept_option)
 
 	if err != nil {
 		log.Fatal(err)
