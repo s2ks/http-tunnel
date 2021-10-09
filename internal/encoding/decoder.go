d51aff1c518285e526afa9a3b9052e7e5b725415
Jilles Bakker <bakkerjilles@gmail.com>
2021-10-09 23:48:24 +0200

    Fixed package name discrepancy

--- internal/encoding/decoder.go
+++ internal/encoding/decoder.go
@@ -1,4 +1,4 @@
-package decoder
+package encoding
 
 import "encoding/base64"
 
--- internal/encoding/encoder.go
+++ internal/encoding/encoder.go
@@ -1,7 +1,7 @@
-package encoder
+package encoding
 
 import "encoding/base64"
 
 func Encode(data []byte) string {
-	return base64.RawURLEncoding.EncodeToString(buf)
+	return base64.RawURLEncoding.EncodeToString(data)
 }
