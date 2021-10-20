ce1db39020cf019bf77d4cd095f9b1b5c5ace876
Jilles Bakker <bakkerjilles@gmail.com>
2021-10-20 15:32:14 +0200

    only return number of bytes written

--- internal/encoding/decoder.go
+++ internal/encoding/decoder.go
@@ -6,7 +6,7 @@ func Decode(data []byte) ([]byte, error) {
 	max_len := base64.RawURLEncoding.DecodedLen(len(data))
 	decoded_buf := make([]byte, max_len)
 
-	_, err := base64.RawURLEncoding.Decode(decoded_buf, data)
+	n, err := base64.RawURLEncoding.Decode(decoded_buf, data)
 
-	return decoded_buf, err
+	return decoded_buf[0:n], err
 }
