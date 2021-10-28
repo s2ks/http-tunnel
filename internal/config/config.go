0d29a08aa508bff250867ec6ad5cf002543f2e7e
Jilles Bakker <bakkerjilles@gmail.com>
2021-10-28 20:29:01 +0200

    Use regular expressions to parse configuration file lines. Is this a good idea? I don't know.
    Also re-indented with spaces instead of tabs, just as an experiment.

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,105 +11,106 @@ connect=addr
 package config
 
 import (
-	"os"
-	"bufio"
-	"strings"
-	"fmt"
-	//"log"
+        "os"
+        "bufio"
+        "strings"
+        "fmt"
+        "regexp"
+        //"log"
+)
+
+var (
+        whitespace      = regexp.MustCompile(`^[[:space:]]+|[[:space:]]+$`)
+
+        section         = regexp.MustCompile(`^\[(.+)\]$`)
+        comment         = regexp.MustCompile(`^;.*|^#.*`)
+        keyval          = regexp.MustCompile(`(^[^[:space:]]+)=(.*)`)
+        blankline       = regexp.MustCompile(`^[[:space:]]*$`)
 )
 
 type Config map[string]Section
 type Section map[string][]string
 
 type Line struct {
-	body 	string
-	no 	int
+        body    string
+        no      int
 }
 
 func ParseKeyVal(line *Line, s Section) error {
-	part := strings.SplitN(line.body, "=", 2)
+        part := strings.SplitN(line.body, "=", 2)
 
-	if len(part) < 2 {
-		return fmt.Errorf("Malformed expression on line: %d" +
-		"\n\t--> \"%s\"", line.no, line.body)
-	}
+        if len(part) < 2 {
+                return fmt.Errorf("Malformed expression on line: %d" +
+                "\n\t--> \"%s\"", line.no, line.body)
+        }
 
-	key := part[0]
-	val := part[1]
+        key := part[0]
+        val := part[1]
 
-	key = strings.Trim(key, " \t\r\n")
-	val = strings.Trim(val, " \t\r\n")
+        key = whitespace.ReplaceAllString(key, "")
+        val = whitespace.ReplaceAllString(val, "")
 
-	s[key] = append(s[key], val)
+        s[key] = append(s[key], val)
 
-	return nil
+        return nil
 }
 
 /* A section has the form of [section name] */
 func ParseSection(line *Line) (string, error) {
-	section_name := ""
+        section_name := section.ReplaceAllString(line.body, "$1")
 
-	/* Assume the caller has made sure that line[0] == '[' */
-	for i := 1; i < len(line.body); i++ {
-		if line.body[i] == ']' {
-			return section_name, nil
-		}
-
-		section_name += string(line.body[i])
-	}
-
-	return section_name, fmt.Errorf("Missing closing bracket ']' " +
-	"in section on line: %d\n\t--> \"%s\"", line.no, line.body)
+        if section_name == "" {
+                return "", fmt.Errorf("Unable to parse section name on line %d: \"%s\"",
+                                line.no, line.body)
+        } else {
+                return section_name, nil
+        }
 }
 
 func Parse(cfg_file *os.File) (Config, error) {
-	reader := bufio.NewReader(cfg_file)
-	config_map := make(Config)
+        reader := bufio.NewReader(cfg_file)
+        config_map := make(Config)
 
-	section_name := "__global"
-	config_map[section_name] = make(Section)
+        section_name := "__global"
+        config_map[section_name] = make(Section)
 
-	var err error
-	var line Line
+        var err error
+        var line Line
 
-	line.no = 0
+        line.no = 0
 
-	for {
-		line.body, err = reader.ReadString('\n')
-		line.no++
+        for {
+                line.body, err = reader.ReadString('\n')
+                line.no++
 
-		if line.body == "" && err != nil {
-			break
-		}
+                if line.body == "" && err != nil {
+                        break
+                }
 
-		line.body = strings.Trim(line.body, " \t\r\n") /* Trim whitespace */
+                /* Trim whitespace at start and end of line */
+                line.body = whitespace.ReplaceAllString(line.body, "")
 
-		if len(line.body) == 0 {
-			continue
-		}
+                switch true {
+                        case blankline.MatchString(line.body):
+                        case comment.MatchString(line.body):
+                        case section.MatchString(line.body):
+                                section_name, err = ParseSection(&line)
 
-		/* Go switch statements don't 'fall thorugh' */
-		switch line.body[0] {
-			/* section */
-			case '[':
-				section_name, err = ParseSection(&line)
+                                if err != nil {
+                                        return nil, err
+                                }
+                                config_map[section_name] = make(Section)
+                        case keyval.MatchString(line.body):
+                                err = ParseKeyVal(&line, config_map[section_name])
 
-				if err != nil {
-					return nil, err
-				}
+                                if err != nil {
+                                        return nil, err
+                                }
+                        default:
+                                return nil, fmt.Errorf("Malformed expression on line " +
+                                                "%d: \"%s\"", line.no, line.body)
+                }
+        }
 
-				config_map[section_name] = make(Section)
-			/* ignore comments */
-			case ';', '#', '\n':
-			default:
-				/* Parse key=val */
-				err = ParseKeyVal(&line, config_map[section_name])
-
-				if err != nil {
-					return nil, err
-				}
-		}
-	}
-
-	return config_map, nil
+        return config_map, nil
 }
