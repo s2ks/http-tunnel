ccee1b16d95b473dec9d1ba2c30fa4d307854c02
Jilles Bakker <bakkerjilles@gmail.com>
2021-12-18 23:33:56 +0100

    Implement decoder as an io.Reader

--- internal/encoding/decoder.go
+++ internal/encoding/decoder.go
@@ -1,6 +1,35 @@
 package encoding
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"io"
+)
+
+type Decoder struct {
+	r io.Reader
+}
+
+func (d *Decoder) Read(dest []byte) (n int, err error) {
+	buflen := base64.RawURLEncoding.EncodedLen(len(dest))
+	buf := make([]byte, buflen)
+
+	n, err = d.r.Read(buf)
+
+	if err != nil {
+		return
+	}
+
+	n, err = base64.RawURLEncoding.Decode(dest, buf[:n])
+
+	return
+}
+
+func NewDecoderFromReader(r io.Reader) (*Decoder) {
+	decoder := Decoder{}
+	decoder.r = r
+
+	return &decoder
+}
 
 func Decode(data []byte) ([]byte, error) {
 	max_len := base64.RawURLEncoding.DecodedLen(len(data))
@@ -8,5 +37,5 @@ func Decode(data []byte) ([]byte, error) {
 
 	n, err := base64.RawURLEncoding.Decode(decoded_buf, data)
 
-	return decoded_buf[0:n], err
+	return decoded_buf[:n], err
 }
